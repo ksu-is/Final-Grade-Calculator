92fa7487ad5fb1a368f213d9c4b23b7696b4d5d7
RyancNg <54372492+RyancNg@users.noreply.github.com>
2019-10-29 14:23:26 -0400

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,20 @@
 print("Hello, welcome to the Will I fail Calculator")
 print('Please Choose a category to start with')
 
-print('Press \n 1 grade quizzes \n 2 grade test \n 3 grade assignments \n 4 grade homework \n for other')
+Grader=input('Press \n 1 grade quizzes \n 2 grade test \n 3 grade assignments \n 4 grade homework \n 5 for other \n 6 for final grade \n ')
+
+if Grader=='1':
+    while True:
+        quiz_weight=input('How much does your quizzes weigh? or if not applicable type n/a ')
+        if quiz_weight.isdigit():
+            print('Please use a decimal')
+        elif quiz_weight.isalpha():
+            print('Please use a decimal')
+        else:
+            while True:
+                quiz_amount=input('How many quizzes have you taken? ')
+                if quiz_amount.isdecimal():
+                    print('Please use a integer')
+                elif quiz_amount.isalpha():
+                    print('Please use a integer')
+                
