500b2266413b1e5767c85d82fc7c0e239e53e27b
RyancNg <54372492+RyancNg@users.noreply.github.com>
2019-10-29 14:07:50 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,4 @@
+print("Hello, welcome to the Will I fail Calculator")
+print('Please Choose a category to start with')
+
+print('Press \n 1 grade quizzes \n 2 grade test \n 3 grade assignments \n 4 grade homework \n for other')
