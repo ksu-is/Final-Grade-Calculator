2c5e8ecff2aa45231f0344dcafaad4826fd3ff2f
RyancNg <54372492+RyancNg@users.noreply.github.com>
2019-10-30 10:29:21 -0400

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -48,16 +48,49 @@ def Test():
                     print('Your test grades are worth',test_final,'%', 'of your grade')
                     Menu()
 
-
+def assignments():
+    while True:
+        assignment_weight=input('How much does your assignments weigh? or if not applicable type n/a ')
+        if assignment_weight.isdigit():
+            print('Please use a decimal')
+        elif assignment_weight.isalpha():
+            print('Please use a decimal')
+        elif assignment_weight=='n/a':
+            Menu()
+        else:
+            while True:
+                assignment_amount=input('How many quizzes have you taken? ')
+                if assignment_amount.isdigit()==False:
+                    print('Please use a integer')
+                else:
+                    assignment_value=[]
+                    for scores in range(int(assignment_amount)):
+                        assignment_scores=int(input('Please insert scores one at a time '))
+                        assignment_value.append(assignment_scores)
+                    assignment_total=sum(assignment_value)
+                    assignment_final=assignment_total/int(assignment_amount)
+                    print('Your assignment grades are worth',assignment_final,'%', 'of your grade')
+                    Menu()
 
 def Menu():
     print("Hello, welcome to the Will I fail Calculator")
     print('Please Choose a category to start with')
-    Grader=input('Press \n 1 grade quizzes \n 2 grade test \n 3 grade assignments \n 4 grade homework \n 5 for other \n 6 for final grade \n ')
+    Grader=input('Press \n 1 Grade quizzes \n 2 Grade test \n 3 Grade assignments \n 4 Grade homework \n 5 Grade Custom Category \n 6 for final grade (Must have all pertainig grades calculated before use) \n ')
     if Grader=='1':
         quizzes()
     elif Grader=='2':
         Test()
+    elif Grader=='3':
+        assignments()
+    elif Grader=='4':
+        homework()
+    elif Grader=='5':
+        custom()
+    elif Grader=='6':
+        final()
+    else:
+        print('that is not one of the options')
+        Menu()
 
 
 Menu()
